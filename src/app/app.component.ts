f57b93cbe6f58bc19a2b8ee961bb80923eda12d1
OjashviSingh <ojashvisingh365@gmail.com>
2023-07-13 16:53:51 +0530

    Added: My Hello World Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-world-angular-app';
+  imgUrl = "../assets/BridgeLabzLogo.svg"
+  url = "https://www.bridgelabz.com"
+  userName: string = "";
+  nameError: string ="";
+
+  ngOnInit():void{
+    this.title = "Hello from BridgeLabz"
+    this.imgUrl = "../assets/BridgeLabzLogo.svg"
+  }
+
+  onClick($event: any){
+    console.log("Save button is clicked", $event)
+    window.open(this.url, "_blank");
+  }
+
+  onInput($event: any){
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex = RegExp('^[A-Z][a-zA-Z]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect";
+  }
 }
